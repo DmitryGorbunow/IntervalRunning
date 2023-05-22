a430971f522c97784d8332b22db12b61d177236a
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2023-05-22 13:28:52 +0500

    Initial Commit

---
+++ IntervalRunning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntervalRunning
+//
+//  Created by Dmitry Gorbunow on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
